aca5eed547a9275cda6cd8584e1b26c90b6409ab
Kuanysh Kalikhanov <mrkunyabai@gmail.com>
2021-07-24 18:42:21 +0600

    Добавил логику категории для книги

--- src/main/java/com/attractor/exam/controller/BookController.java
+++ src/main/java/com/attractor/exam/controller/BookController.java
@@ -1,17 +1,22 @@
 package com.attractor.exam.controller;
 
 import com.attractor.exam.model.Book;
+import com.attractor.exam.model.Category;
 import com.attractor.exam.repository.BookRepository;
+import com.attractor.exam.repository.CategoryRepository;
 import com.attractor.exam.service.BookService;
 import com.attractor.exam.service.FileSystemStorageService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,21 +28,23 @@ public class BookController {
     private final BookRepository bookRepository;
     private final BookService bookService;
     private final FileSystemStorageService storageService;
+    private final CategoryRepository categoryRepository;
 
-    public BookController(BookRepository bookRepository, BookService bookService, FileSystemStorageService storageService) {
+    public BookController(BookRepository bookRepository, BookService bookService, FileSystemStorageService storageService, CategoryRepository categoryRepository) {
         this.bookRepository = bookRepository;
         this.bookService = bookService;
         this.storageService = storageService;
+        this.categoryRepository = categoryRepository;
     }
 
     @GetMapping
-    public String getBooks(Model model, @RequestParam(defaultValue = "1", required = false) int page){
+    public String getBooks(Model model, @RequestParam(defaultValue = "1", required = false) int page) {
         int pageSize = 8;
         int currentPage = page - 1;
-        if(currentPage < 0)
+        if (currentPage < 0)
             currentPage = 0;
 
-        Pageable pageable = PageRequest.of(currentPage, pageSize);
+        Pageable pageable = PageRequest.of(currentPage, pageSize, Sort.by(Sort.Order.desc("addingDate")));
         Page<Book> pageBooks = bookRepository.findAll(pageable);
         List<Book> books = pageBooks.toList();
         model.addAttribute("hasNext", pageBooks.hasNext());
@@ -48,7 +55,9 @@ public class BookController {
     }
 
     @GetMapping("/add")
-    public String getBookAddPage(){
+    public String getBookAddPage(Model model) {
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
         return "bookAdd";
     }
 
@@ -56,11 +65,16 @@ public class BookController {
     public String addBook(@RequestParam String name,
                           @RequestParam String author,
                           @RequestParam MultipartFile file,
-                          @RequestParam (required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate yearOfIssue,
-                          @RequestParam String description){
-        if(yearOfIssue == null)
+                          @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate yearOfIssue,
+                          @RequestParam String categoryId,
+                          @RequestParam String description) {
+        if (yearOfIssue == null)
             yearOfIssue = LocalDate.now();
-        Book book = new Book(name, author, file.getOriginalFilename(), yearOfIssue, description);
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                String.format("Категория с id %s не найден", categoryId)
+        ));
+        Book book = new Book(name, author, file.getOriginalFilename(), yearOfIssue, description, category);
         storageService.store(file);
         bookRepository.save(book);
         return "redirect:/";
@@ -68,7 +82,7 @@ public class BookController {
 
     @GetMapping("/{id}")
     public String getBook(Model model,
-                              @PathVariable String id){
+                          @PathVariable String id) {
         Book book = bookService.getById(id);
         model.addAttribute("book", book);
         return "bookInfo";
@@ -76,7 +90,7 @@ public class BookController {
 
     @GetMapping("/{id}/update")
     public String getUpdateBook(Model model,
-                                @PathVariable String id){
+                                @PathVariable String id) {
         Book book = bookService.getById(id);
         model.addAttribute("book", book);
         return "bookUpdate";
@@ -86,9 +100,9 @@ public class BookController {
     public String updateCategory(@PathVariable String id,
                                  @RequestParam String name,
                                  @RequestParam String author,
-                                 @RequestParam (required = false)MultipartFile file,
+                                 @RequestParam(required = false) MultipartFile file,
                                  @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate yearOfIssue,
-                                 @RequestParam String description){
+                                 @RequestParam String description) {
 
 
         storageService.store(file);
@@ -104,20 +118,17 @@ public class BookController {
 
     @GetMapping("/{id}/deletePage")
     public String getDeleteCategory(
-            @PathVariable String id){
+            @PathVariable String id) {
         return "bookDelete";
     }
 
     @GetMapping("/{id}/delete")
     public String deleteBook(
-            @PathVariable String id){
+            @PathVariable String id) {
         Book book = bookService.getById(id);
         bookRepository.delete(book);
         return "redirect:/";
     }
 
-    @GetMapping("/authorization")
-    public String getAuthorization(){
-        return "authorization";
-    }
+
 }
--- src/main/java/com/attractor/exam/model/Book.java
+++ src/main/java/com/attractor/exam/model/Book.java
@@ -2,9 +2,12 @@ package com.attractor.exam.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Document(collection = "books")
@@ -17,17 +20,21 @@ public class Book {
     private String photo;
     private LocalDate yearOfIssue;
     private String description;
-    private LocalDate addingDate;
+    private LocalDateTime addingDate;
     private String status;
 
-    public Book(String name, String author, String photo, LocalDate yearOfIssue, String description) {
+    @DBRef
+    private Category category;
+
+    public Book(String name, String author, String photo, LocalDate yearOfIssue, String description, Category category) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.author = author;
         this.photo = photo;
         this.yearOfIssue = yearOfIssue;
         this.description = description;
-        this.addingDate = LocalDate.now();
+        this.addingDate = LocalDateTime.now(Clock.systemUTC());
         this.status = "В наличии";
+        this.category = category;
     }
 }
